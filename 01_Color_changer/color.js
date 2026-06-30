d98e012bd02dcc4fd292c23f234d5b9849d749a3
sujeet yadav <sujityadav595@gmail.com>
2026-06-30 20:48:20 +0530

    my 2nd commit

--- 01_Color_changer/color.js
+++ 01_Color_changer/color.js
@@ -66,46 +66,72 @@
 // =================== My 3rd  Code ===========================
 
 
-const button = document.querySelectorAll('.button');
- const body = document.querySelector('body')
+// const button = document.querySelectorAll('.button');
+//  const body = document.querySelector('body')
 
 
-//  console.log(button);
+// //  console.log(button);
 
-button.forEach(function(button){
+// button.forEach(function(button){
 
-    // console.log(button);
+//     // console.log(button);
 
-    button.addEventListener('click', function(e){
+//     button.addEventListener('click', function(e){
 
-        // console.log(e.target);
-        if(e.target.id === 'grey'){
-            body.style.backgroundColor = e.target.id
-            console.log(e.target.id);
+//         // console.log(e.target);
+//         if(e.target.id === 'grey'){
+//             body.style.backgroundColor = e.target.id
+//             console.log(e.target.id);
             
-        }
-        if(e.target.id === 'white'){
-            body.style.backgroundColor = e.target.id
-            console.log(e.target.id);
-        }
-        if(e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id
-            console.log(e.target.id);
-        }
-        if(e.target.id === 'yellow'){
-            body.style.backgroundColor = e.target.id
-            console.log(e.target.id);
-        }
+//         }
+//         if(e.target.id === 'white'){
+//             body.style.backgroundColor = e.target.id
+//             console.log(e.target.id);
+//         }
+//         if(e.target.id === 'blue'){
+//             body.style.backgroundColor = e.target.id
+//             console.log(e.target.id);
+//         }
+//         if(e.target.id === 'yellow'){
+//             body.style.backgroundColor = e.target.id
+//             console.log(e.target.id);
+//         }
                    
-    })
+//     })
     
-})
+// })
  
 
 
 
+// =================== My 4th  Code ===========================
 
 
+const button = document.querySelectorAll('.button')
+const body = document.querySelector('body');
+
+
+button.forEach(function(button){
+    // console.log(button);
+
+    button.addEventListener('click',function(e){
+        console.log(e.target.id);
+
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id
+        }else if(e.target.id === 'pink'){
+            body.style.backgroundColor = e.target.id
+        }else if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id
+        }else if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id
+        }else{
+
+        }
+        
+    });
+    
+})
 
 
 

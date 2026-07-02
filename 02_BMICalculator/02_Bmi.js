14ff16d5c9558863949367170c0b5634e5299352
sujeet yadav <sujityadav595@gmail.com>
2026-07-02 22:15:05 +0530

    add project pdf

--- 02_BMICalculator/02_Bmi.js
+++ 02_BMICalculator/02_Bmi.js
@@ -23,3 +23,36 @@
 
 
 // ======================= Code 2 =========================
+const form = document.querySelector('form');
+
+
+form.addEventListener('submit', function (e) {
+
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+
+    const result = document.querySelector('#results');
+    // console.log(height);
+
+
+
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        result.innerHTML = `please enter a valid height${height}`;
+
+
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = `please enter a valid weight ${weight}`;
+    } else {
+        const bmi = (weight / ((height * height) / 1000)).toFixed(2)
+
+        result.innerHTML = `<span>${bmi}<span>`
+    }
+
+
+
+
+
+})
